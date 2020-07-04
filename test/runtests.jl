09b2e93f193a07723074cf1ec8185cdb9730dd1e
Saketh Rama <rama@seas.harvard.edu>
2020-07-04 17:34:12 -0400

    test: add pareto plot test for coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using ParetoRecipes
 using Test
 
-@testset "ParetoRecipes.jl" begin
-    @test pareto_front([[1, 0], [0, 1], [1, 1], [2, 2]]) == [[0, 1], [1, 0]]
+fourpoints = [[1, 0], [0, 1], [1, 1], [2, 2]]
+
+@testset "pareto front" begin
+    @test pareto_front(fourpoints) == [[0, 1], [1, 0]]
+end
+
+@testset "pareto plot" begin
+    m = ParetoRecipes.Pareto(fourpoints)
+    @test m.args == fourpoints
 end
